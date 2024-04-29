9986090fcce3abefa90c80dfd99aa2a123ed533d
Ryan Graebner <ryan.graebner@gmail.com>
2024-04-29 15:27:48 -0700

    Reduced Color Options

--- lib/utilities/config_service.dart
+++ lib/utilities/config_service.dart
@@ -8,8 +8,6 @@ class Configuration {
   Color primaryColor;
   Color secondaryColor;
   Color dividerColor;
-  Color fixedColumnColor;
-  Color appBarTextColor;
   List<String> pageTitles;
   String dataSummaryURL;
   String dataSummaryText;
@@ -18,10 +16,8 @@ class Configuration {
     final indexUrl = configString['indexURL'] ?? "https://cropandsoil.oregonstate.edu/sites/agscid7/files/cbarc/variety-testing/index.csv";
 
     Color primaryColor = const Color(0xFFD73F09),
-        secondaryColor = const Color(0xFFEEEEEE),
-        dividerColor = const Color(0xFF8E8E8E),
-        fixedColumnColor = Colors.grey.withOpacity(0.3),
-        appBarTextColor = const Color(0xFFFFFFFF);
+        secondaryColor = const Color(0xFFFFFFFF),
+        dividerColor = const Color(0xFF8E8E8E);
 
     if (configString['primaryColor'] != null) {
       primaryColor = Color(
@@ -35,14 +31,6 @@ class Configuration {
       dividerColor = Color(
           int.parse(configString['dividerColor'], radix: 16) + 0xFF000000);
     }
-    if (configString['fixedColumnColor'] != null) {
-      fixedColumnColor = Color(
-      int.parse(configString['fixedColumnColor'], radix: 16) + 0xFF000000);
-    }
-    if (configString['appBarTextColor'] != null) {
-      appBarTextColor = Color(
-          int.parse(configString['appBarTextColor'], radix: 16) + 0xFF000000);
-    }
     print(dividerColor);
 
     List<String> titles = [];
@@ -54,8 +42,8 @@ class Configuration {
                     These trials serve as the final testing ground for varieties developed by universities, the USDA, and private breeding breeding programs.\n\n
                     The data within this app is from the OSU cereal variety testing regional summaries and disease summaries, which are available online at:""";
 
-    return Configuration(configString, indexUrl, primaryColor, secondaryColor, dividerColor, fixedColumnColor, appBarTextColor, titles, dataSummaryURL, dataSummaryText);
+    return Configuration(configString, indexUrl, primaryColor, secondaryColor, dividerColor, titles, dataSummaryURL, dataSummaryText);
   }
 
-  Configuration(this.configString, this.indexUrl, this.primaryColor, this.secondaryColor, this.dividerColor, this.fixedColumnColor, this.appBarTextColor, this.pageTitles, this.dataSummaryURL, this.dataSummaryText);
+  Configuration(this.configString, this.indexUrl, this.primaryColor, this.secondaryColor, this.dividerColor, this.pageTitles, this.dataSummaryURL, this.dataSummaryText);
 }
