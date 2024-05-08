9fc32df92f0187cb84bf17c89ccfedd0bfeaa3ea
Ryan Graebner <ryan.graebner@gmail.com>
2024-05-08 08:15:01 -0700

    Added Trait Descriptions

--- lib/utilities/config_service.dart
+++ lib/utilities/config_service.dart
@@ -10,6 +10,7 @@ class Configuration {
   List<String> pageTitles;
   String dataSummaryURL;
   String dataSummaryText;
+  String dataTraitDescriptions;
 
   factory Configuration.fromMap(Map<String, dynamic> configString) {
     final indexUrl = configString['indexURL'] ?? "https://cropandsoil.oregonstate.edu/sites/agscid7/files/cbarc/variety-testing/index.csv";
@@ -35,9 +36,11 @@ class Configuration {
     String dataSummaryText = configString['dataSummaryText'] ?? """Every year, the Oregon State University Cereal Extension Program conducts wheat and barley variety trials across the state.
                     These trials serve as the final testing ground for varieties developed by universities, the USDA, and private breeding breeding programs.\n\n
                     The data within this app is from the OSU cereal variety testing regional summaries and disease summaries, which are available online at:""";
+    String dataTraitDescriptions = configString['dataTraitDescriptions'] ?? """Disease Resistance Scores:\nR - Resistant\nMR - Moderately Resistant\nI - Intermediate\nMS - Moderately Susceptible\n
+                    S - Susceptible\n\nQuality Scores:\nMD - Most Desirable\nD - Desirable\nA - Acceptable\nLD - Least Desirable""";
 
-    return Configuration(configString, indexUrl, primaryColor, dividerColor, titles, dataSummaryURL, dataSummaryText);
+    return Configuration(configString, indexUrl, primaryColor, dividerColor, titles, dataSummaryURL, dataSummaryText, dataTraitDescriptions);
   }
 
-  Configuration(this.configString, this.indexUrl, this.primaryColor, this.dividerColor, this.pageTitles, this.dataSummaryURL, this.dataSummaryText);
+  Configuration(this.configString, this.indexUrl, this.primaryColor, this.dividerColor, this.pageTitles, this.dataSummaryURL, this.dataSummaryText, this.dataTraitDescriptions);
 }
